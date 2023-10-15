33d99078d324e70f5dc26970d8b032dc7f8c60ff
JavanFriedel <javanfriedel@hotmail.com>
2023-10-14 22:06:46 -0400

    Finished implements all requested function and testing

--- index.js
+++ index.js
@@ -1,6 +1,81 @@
-export function capitalize(string) {
+function capitalize(string) {
   const stringSplit = string.split('');
   stringSplit[0] = stringSplit[0].toUpperCase();
 
   return stringSplit.join('');
 }
+
+function reverseString(string) {
+  return string.split('').reverse().join('');
+}
+
+function calculator() {
+  function subtract(num1, num2) {
+    return num1 - num2;
+  }
+
+  function divide(num1, num2) {
+    return num1 / num2;
+  }
+
+  function multiply(num1, num2) {
+    return num1 * num2;
+  }
+
+  return {
+    subtract,
+    divide,
+    multiply,
+  };
+}
+
+function caesarCipher(string, offset) {
+  const cipherArray = [];
+  const originString = string.split('');
+
+  for (let i = 0; i < originString.length; i++) {
+    cipherArray.push(originString[i].charCodeAt(0) + offset);
+  }
+
+  return String.fromCharCode(...cipherArray);
+}
+
+function analyzeArray(array) {
+  let min, max;
+  const length = array.length;
+  const average =
+    array.reduce((sum, element, index) => {
+      if (index == 0) {
+        min = element;
+        max = element;
+      }
+
+      if (element < min) {
+        min = element;
+      }
+
+      if (element > max) {
+        max = element;
+      }
+
+      return sum + element;
+    }, 0) / length;
+
+  return {
+    average,
+    min,
+    max,
+    length,
+  };
+}
+
+// console.log(caesarCipher('abcde', 1));
+console.log(analyzeArray([1, 8, 3, 4, 2, 6]));
+
+module.exports = {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+};
--- tests.test.js
+++ tests.test.js
@@ -1,5 +1,56 @@
-import { capitalize } from './index';
+const functions = require('./index.js');
 
-test('First Test against Capitalize', () => {
+const capitalize = functions.capitalize;
+const reverseString = functions.reverseString;
+const calculator = functions.calculator;
+const caesarCipher = functions.caesarCipher;
+const analyzeArray = functions.analyzeArray;
+
+// analyzeArray,
+
+test('Capitalize: javan should equal Javan', () => {
   expect(capitalize('javan')).toBe('Javan');
+});
+
+test('Capitalize: big should equal Big', () => {
+  expect(capitalize('big')).toBe('Big');
+});
+
+test('Reverse String: Dog should equal goD', () => {
+  expect(reverseString('Dog')).toBe('goD');
+});
+
+test('Reverse String: reverse should equal esrever', () => {
+  expect(reverseString('reverse')).toBe('esrever');
+});
+
+test('Calculator: Dog should equal goD', () => {
+  expect(calculator().subtract(3, 1)).toBe(2);
+});
+
+test('Calculator: Dog should equal goD', () => {
+  expect(calculator().divide(4, 2)).toBe(2);
+});
+
+test('Calculator: Dog should equal goD', () => {
+  expect(calculator().multiply(5, 5)).toBe(25);
+});
+
+test('Cipher: Small Codes with no wrapping', () => {
+  expect(caesarCipher('abcde', 1)).toBe('bcdef');
+});
+
+test('Cipher: Long with no wrapping', () => {
+  expect(caesarCipher('abcdefghijklmnopqrstuvwxy', 1)).toBe(
+    'bcdefghijklmnopqrstuvwxyz'
+  );
+});
+
+test('Analyze Array: [1,8,3,4,2,6]', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
